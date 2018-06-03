cfd1a0da8ef0864c90926249b9c6b1f1fb5e2f94
mastracu <mastracu@fastmail.co.uk>
2018-06-03 10:45:11 +0200

    Initial commit

---
+++ Droid/MainActivity.fs
@@ -0,0 +1,27 @@
+﻿namespace FsXamarinForms.Droid
+open System
+
+open Android.App
+open Android.Content
+open Android.Content.PM
+open Android.Runtime
+open Android.Views
+open Android.Widget
+open Android.OS
+open Xamarin.Forms.Platform.Android
+
+type Resources = FsXamarinForms.Droid.Resource
+
+[<Activity (Label = "FsXamarinForms.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
+type MainActivity() =
+    inherit FormsAppCompatActivity()
+
+    override this.OnCreate (bundle: Bundle) =
+        FormsAppCompatActivity.TabLayoutResource <- Resources.Layout.Tabbar
+        FormsAppCompatActivity.ToolbarResource <- Resources.Layout.Toolbar
+
+        base.OnCreate (bundle)
+
+        Xamarin.Forms.Forms.Init (this, bundle)
+
+        this.LoadApplication (new FsXamarinForms.App ())
